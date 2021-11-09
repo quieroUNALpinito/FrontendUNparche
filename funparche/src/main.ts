cbc48cf52b24419d4e85684ac5f1c9f8bdc17130
jsromero98 <84394100+jsromero98@users.noreply.github.com>
2021-11-08 19:34:48 -0500

    agregada primera version de pagina about en la app

--- funparche/src/main.ts
+++ funparche/src/main.ts
@@ -1,6 +1,26 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './registerServiceWorker'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+import PrimeVue from 'primevue/config'
+import Steps from 'primevue/steps'
+import Button from 'primevue/button'
+import MegaMenu from 'primevue/megamenu'
+import InputText from 'primevue/inputtext'
+
+import 'primeflex/primeflex.css'
+import 'primevue/resources/themes/saga-purple/theme.css'
+import 'primevue/resources/primevue.css'
+import 'primeicons/primeicons.css'
+
+const app = createApp(App)
+
+app.use(PrimeVue)
+
+app.component('Steps', Steps)
+app.component('Button', Button)
+app.component('MegaMenu', MegaMenu)
+app.component('InputText', InputText)
+
+app.use(router)
+app.mount('#app')
