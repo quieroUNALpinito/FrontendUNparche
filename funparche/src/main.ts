9ebd4cab0cde6001048e6bdb81a0e8c7ff810702
nsochoaga <nsochoaga@unal.edu.co>
2021-11-15 19:58:32 -0500

    Crear la vista para consultar eventos por hora.

--- funparche/src/main.ts
+++ funparche/src/main.ts
@@ -2,5 +2,21 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
+import PrimeVue from 'primevue/config'
+import Accordion from 'primevue/accordion'
+import AccordionTab from 'primevue/accordiontab'
+import Card from 'primevue/card'
+import Splitter from 'primevue/splitter'
+import SplitterPanel from 'primevue/splitterpanel'
+import Calendar from 'primevue/calendar'
+import OrderList from 'primevue/orderlist'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+app.use(PrimeVue)
+app.component('Accordion', Accordion)
+app.component('AccordionTab', AccordionTab)
+app.component('Card', Card)
+app.component('Splitter', Splitter)
+app.component('SplitterPanel', SplitterPanel)
+app.component('Calendar', Calendar).component('OrderList', OrderList)
+app.use(router).mount('#app')
---
+++ funparche/src/views/ConsultarEvento.vue
@@ -0,0 +1,89 @@
+<template>
+  <Card id="filtro">
+    <template #title>
+      <h2>Filters</h2>
+    </template>
+    <template #content>
+      <Accordion :multiple="true" :activeIndex="0">
+        <AccordionTab>
+          <template #header>
+            <i class="pi pi-calendar"></i>
+            <h3>Date</h3>
+          </template>
+          Content
+        </AccordionTab>
+        <AccordionTab>
+          <template #header>
+            <i class="pi pi-calendar"></i>
+            <h3>Time</h3>
+          </template>
+          <span>Escoge la hora libre o el rango de tiempo libre.</span><br />
+          <input type="time" />
+          <input type="time" />
+        </AccordionTab>
+        <AccordionTab>
+          <template #header>
+            <i class="pi pi-calendar"></i>
+            <h3>Another filter</h3>
+          </template>
+          Content
+        </AccordionTab>
+      </Accordion>
+    </template>
+  </Card>
+  <Card id="Lista">
+    <template #title>
+      <h2>List of Events</h2>
+    </template>
+    <template #content>
+      <div>
+        <table class="table">
+          <tr>
+            <th>Nombre de Evento</th>
+            <th>Lugar</th>
+            <th>Hora de inicio</th>
+          </tr>
+          <tr v-for="item in lista" :key="item.nombre">
+            <td v-text="item.Evento"></td>
+            <td v-text="item.lugar"></td>
+            <td v-text="item.hora"></td>
+          </tr>
+        </table>
+      </div>
+    </template>
+  </Card>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      value_time: null,
+      lista: [
+        {
+          Evento: 'Nombre del evento',
+          lugar: 'Lugar del evento',
+          hora: 'hora de inicio'
+        },
+        {
+          Evento: 'Nombre del evento',
+          lugar: 'Lugar del evento',
+          hora: 'hora de inicio'
+        },
+        {
+          Evento: 'Nombre del evento',
+          lugar: 'Lugar del evento',
+          hora: 'hora de inicio'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+#filtro {
+  width: 25%;
+  float: left;
+}
+</style>
