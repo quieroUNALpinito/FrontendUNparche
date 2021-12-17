b888db64e4918e575a5f48078c8e03341d15119e
daniel <damorenor@unal.edu.co>
2021-12-17 13:05:07 -0500

    get de edificios con locacion implementado

--- funparche/src/components/UbicacionEvento.vue
+++ funparche/src/components/UbicacionEvento.vue
@@ -2,13 +2,15 @@
   <div>
     <Button label="Ver Mapa" icon="pi pi-external-link" @click="openResponsive" />
     <Dialog header="Consultar Evento por Ubicación" v-model:visible="displayResponsive" :breakpoints="{'960px': '75vw'}" :style="{width: '50vw'}">
-      <GoogleMap
-        api-key="Aqui va la API Key!!"
-        style="width: 100%; height: 500px"
-        :center="center"
-        :zoom="16"
-      >
-      </GoogleMap>
+      <Fieldset legend="Lugar">
+        <GoogleMap
+          api-key="AIzaSyAZ2nx6WG9ZGb-ydvhlCdUarqVsOhGuCn4"
+          style="width: 100%; height: 500px;"
+          :center="center"
+          :zoom="16"
+        >
+        </GoogleMap>
+      </Fieldset>
         <template #footer>
             <Button label="Cancelar" icon="pi pi-times" @click="closeResponsive" class="p-button-text"/>
             <Button label="Buscar" icon="pi pi-search" @click="closeResponsive" autofocus />
@@ -18,13 +20,35 @@
 </template>
 
 <script>
+import axios from 'axios'
 import { ref } from 'vue'
 import { GoogleMap } from 'vue3-google-map'
 
 export default {
   name: 'UbicacionEvento',
+  data () {
+    return {
+      edificios: [],
+      edificio: ''
+    }
+  },
   components: {
     GoogleMap
+  },
+  methods: {
+    loadEdificiosOficiales: function () {
+      console.log('cargando edificios oficiales')
+      axios
+        .get('http://localhost:8080/api/recursos/edificiosLugaresOficiales')
+        .then((response) => {
+          this.edificios = response.data.data
+          console.log(this.edificios)
+        })
+        .catch(function (error) {
+          // handle error
+          console.log(error)
+        })
+    }
   },
   setup () {
     const displayResponsive = ref(false)
@@ -37,12 +61,16 @@ export default {
     }
 
     const center = { lat: 4.6365772, lng: -74.0825521 }
+
     return {
       displayResponsive,
       openResponsive,
       closeResponsive,
       center
     }
+  },
+  mounted: function () {
+    this.loadEdificiosOficiales()
   }
 }
 </script>
--- funparche/src/main.ts
+++ funparche/src/main.ts
@@ -36,6 +36,7 @@ import VueAxios from 'vue-axios'
 import Avatar from 'primevue/avatar'
 import Menubar from 'primevue/menubar'
 import Dialog from 'primevue/dialog'
+import Fieldset from 'primevue/fieldset'
 
 const app = createApp(App)
 
@@ -64,6 +65,7 @@ app.component('InlineMessage', InlineMessage)
 app.component('Avatar', Avatar)
 app.component('Menubar', Menubar)
 app.component('Dialog', Dialog)
+app.component('Fieldset', Fieldset)
 
 const gauthClientId =
   '832086360772-parslg4nd9jl5shdoats8rhucep5tmfv.apps.googleusercontent.com'
